77df4db56a60445ad2add48f4c04e29070af9b00
m1ghtfr3e <lacreaturesceptique@riseup.net>
2021-01-14 23:25:45 +0100

    added TUI

--- setup.py
+++ setup.py
@@ -2,13 +2,26 @@ import setuptools
 
 
 setuptools.setup(
-    name='fitpy',
+    name='enbyfit',
     version='0.0.1.dev1',
     author='m1ghtfr3e',
     description='Fitness and Health',
-    url='',
+    url='https://github.com/m1ghtfr3e/enbyfit',
     packages=setuptools.find_packages(),
+    install_requires=[
+        'npyscreen==4.10.5',
+        'six==1.15.0',
+        'SQLAlchemy==1.3.22',
+        'SQLAlchemy-Utils==0.36.8'
+    ],
     classifiers=[
-        'Programming :: Language :: 3',
-        ]
+        'Operating System :: OS Independent',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Other Audience',
+        'Natural Language :: English',
+        'Topic :: Utilities'
+        ],
+    python_requires='>=3.9',
     )
