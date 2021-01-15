ae503b9872647768b004d0b5faebe0572d13eb1f
m1ghtfr3e <lacreaturesceptique@riseup.net>
2021-01-15 15:21:24 +0100

    : moved tests

---
+++ tests/test_body.py
@@ -0,0 +1,13 @@
+import unittest
+from enbyfit import Body
+
+# Order of params
+# Age, Height, Weight, Hormonak Sex, Waist, Hip
+test_body1 = Body(20, 170, 70, 'male')
+test_body2 = Body(30, 180, 80, 'female')
+
+
+class BasicBodyTests(unittest.TestCase):
+    def test_bmi(self):
+        bmi1 = test_body1.bmi
+        bmi2 = test_body2.bmi
