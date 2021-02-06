ce8e4901a413113e9648c4d5053a3d037a98182f
m1ghtfr3e <lacreaturesceptique@riseup.net>
2021-02-06 22:01:06 +0100

    edited Models for DB

--- enbyfit/models.py
+++ enbyfit/models.py
@@ -5,27 +5,40 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class FitUser(Base):
+class UserDB(Base):
     '''
-        Model of User
-    Model class of the User
+        Model of User Database
     '''
     __tablename__ = 'UserDB'
     id = Column(Integer, primary_key=True)
     date = Column(DateTime)
     dataset = Column(JSON, nullable=False)
 
-    def __repr__(self):
-        return f'FitUser(id={self.id}, date={self.date}, data={self.dataset})'
+    def __repr__(self) -> None:
+        return f'{self.__class__.__name__}: ID {self.id}'
 
 
-class BodyProperties(Base):
-    __tablename__ = 'BodyPropertyDB'
+class BodyDB(Base):
+    '''
+        Model of Body Database
+    '''
+    __tablename__ = 'BodyDB'
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime)
+    data = Column(JSON, nullable=False)
 
-    ...
+    def __repr__(self) -> None:
+        return f'{self.__class__.__name__}: ID {self.id}'
 
 
-class Activities(Base):
+class ActivityDB(Base):
+    '''
+        Model of Acitvity Database
+    '''
     __tablename__ = 'AcitivtyDB'
+    id = Column(Integer, primary_key=True)
+    date = Column(DateTime)
+    data = Column(JSON, nullable=False)
 
-    ...
+    def __repr__(self) -> None:
+        return f'{self.__class__.__name__}: ID {self.id}'
